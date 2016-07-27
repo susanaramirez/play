bb92095593991d74801e9c3eb588b156415313bc
susanaramirez <ramirez.susana.b@gmail.com>
2016-07-26 21:11:30 -0700

    new coding

---
+++ calculator.py
@@ -0,0 +1,33 @@
+def add(num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num1 - num2 
+
+def multiply(num1, num2):
+	return num1 * num2 
+
+def divide(num1, num2):
+	return num1 / num2 
+
+def modulo(num1,num2):
+	return num1 % num2
+
+def power(base, exponent):
+	return base ** exponent
+
+def square(num):
+	return num * num
+
+#calculations
+
+print "a.", add(4, 5) + subtract(9, 6)
+
+print "b.", divide(12, 2) - 60
+
+print "c.", 1 + 2 + 3
+
+print "d.", power(add(1, 2), 2)
+
+print "e. ", divide(modulo(3,4), square(9))
+
